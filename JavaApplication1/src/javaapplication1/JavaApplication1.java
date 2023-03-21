e10f218d7fb7ce5dd846eefff712a61884b7a5cf
Gabriel Almeida <128440479+Brielalmeida@users.noreply.github.com>
2023-03-21 11:11:53 -0300

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,30 @@
+
+package javaapplication1;
+import java.util.Scanner;
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+       int aluno = 1;
+       double idade, soma_idade=0;
+       int cnt = 0;
+       Scanner entrada = new Scanner(System.in);
+       do
+        {
+            System.out.println("Aluno "+ aluno);
+            System.out.print("Digite a sua idade: ");
+            idade = entrada.nextDouble();
+            if (idade >= 16){
+                cnt++;
+            } else {
+                soma_idade += idade;
+            }
+            aluno ++;
+        } while (aluno <= 10);
+       System.out.println("A quantidade de alunos que podem ser eleitores é: "+ cnt);
+       double media = soma_idade/(double)(10-cnt);
+       System.out.println("A média das idades dos alunos que não podem ser eleitores é; "+ media);
+       
+       
+    }
+    
+}
